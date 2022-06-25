43101b1ccc87e7c968495d355810169c46aeaf43
karenhirayama <karenm.hirayama@gmail.com>
2022-06-25 16:31:03 -0300

    Create routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Footer, Navbar } from './components';
+import { Home, NotFound, Stock } from './pages';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/nasdaq-stock/:stock_name' element={<Stock />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+      <Footer />
+    </Router>
+  </div>
+);
 
 export default App;
---
+++ src/components/index.ts
@@ -0,0 +1,3 @@
+export { default as Footer } from "./Footer/Footer";
+export { default as Loading } from "./Loading/Loading";
+export { default as Navbar } from "./Navbar/Navbar";
