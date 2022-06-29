b71ab0c6e176f1536bcd6a93eba3331937d5f9b5
karenhirayama <karenm.hirayama@gmail.com>
2022-06-29 17:20:56 -0300

    Handle error in fetch data

--- src/layouts/Chart/Chart.tsx
+++ src/layouts/Chart/Chart.tsx
@@ -3,35 +3,72 @@ import { useState, useEffect } from 'react';
 import { getStockHistoricalData, getStockHistoricalByDate } from '../../api/api';
 import { HistoricalPrice, RangeTime } from "./components";
 import { useParams } from 'react-router-dom';
+import { Loading } from '../../components';
 
 const Chart = () => {
   const [historicalPrice, setHistoricalPrice] = useState<any>([]);
-  const [ searchByDate, setSearchByDate] = useState<any>({
+  const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [searchByDate, setSearchByDate] = useState<any>({
     initialDate: new Date().toISOString().substr(0, 10) as any,
     finalDate: new Date().toISOString().substr(0, 10) as any
   });
   const params = useParams();
 
   const getHistoricalPrice = async () => {
-    const { data } = await axios.get(getStockHistoricalData(params?.stock_symbol as string));
-    setHistoricalPrice(data.data)
+    try {
+      const { data } = await axios.get(getStockHistoricalData(params?.stock_symbol as string));
+      setHistoricalPrice(data.data);
+      setIsLoading(false);
+    } catch (error) {
+      setIsError(true);
+      setIsLoading(false);
+    }
   };
 
   const getHistoricalPriceByDate = async () => {
-    const { data } = await axios.get(getStockHistoricalByDate(params?.stock_symbol as string, searchByDate.initialDate, searchByDate.finalDate));
-    setHistoricalPrice(data.data);
+    setIsLoading(true);
+    try {
+      const { data } = await axios.get(getStockHistoricalByDate(params?.stock_symbol as string, searchByDate.initialDate, searchByDate.finalDate));
+      console.log(getStockHistoricalByDate(params?.stock_symbol as string, searchByDate.initialDate, searchByDate.finalDate))
+      setHistoricalPrice(data.data);
+      setIsLoading(false)
+    } catch (error) {
+      setIsError(true);
+      setIsLoading(false);
+    }
   }
 
   useEffect(() => {
     getHistoricalPrice();
   }, [params]);
 
-  return (
-    <div>
-      <RangeTime searchByDate={searchByDate} setSearchByDate={setSearchByDate} getHistoricalPriceByDate={getHistoricalPriceByDate} />
-      <HistoricalPrice historicalPrice={historicalPrice} stockName={params?.stock_name} />
-    </div>
-  )
+  if (isLoading) {
+    return (<>
+      <Loading />
+    </>)
+  } else if (isError) {
+    return (
+      <>
+        Error
+      </>
+    )
+  } else {
+    return (
+      <div>
+        <RangeTime
+          searchByDate={searchByDate}
+          setSearchByDate={setSearchByDate}
+          getHistoricalPriceByDate={getHistoricalPriceByDate}
+          handleClearSearch={getHistoricalPrice}
+        />
+        <HistoricalPrice
+          historicalPrice={historicalPrice}
+          stockName={params?.stock_name}
+        />
+      </div>
+    )
+  }
 }
 
 export default Chart;
--- src/layouts/Chart/components/RangeTime/RangeTime.tsx
+++ src/layouts/Chart/components/RangeTime/RangeTime.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react";
 
-const RangeTime: FC<any> = ({ searchByDate, setSearchByDate, getHistoricalPriceByDate }) => {
+const RangeTime: FC<any> = ({ searchByDate, setSearchByDate, getHistoricalPriceByDate, handleClearSearch }) => {
 
   const handleSearchChange = (e: any) => {
     setSearchByDate((searchByDate: any) => ({ ...searchByDate, [e.target.name]: e.target.value }) as any);
@@ -11,17 +11,18 @@ const RangeTime: FC<any> = ({ searchByDate, setSearchByDate, getHistoricalPriceB
       <div className="rangeTime">
         <div className="rangeTime__input">
           <h4>Search by time range</h4>
-          <div className="rangeTime__input__initial_date">
+          <div className="rangeTime__input__section">
             <label htmlFor='initialDate'>Initial date</label>
             <input type='date' id='initialDate' name='initialDate' value={searchByDate.initialDate} onChange={handleSearchChange} />
           </div>
-          <div className="rangeTime__input__final_date">
-            <label htmlFor='finalDate'>Initial date</label>
+          <div className="rangeTime__input__section">
+            <label htmlFor='finalDate'>Final date</label>
             <input type='date' name='finalDate' value={searchByDate.finalDate} onChange={handleSearchChange} />
           </div>
-          <div className="rangeTime__btn">
-            <button className="rangeTime__btn__confirm" onClick={getHistoricalPriceByDate}>Search by date</button>
-          </div>
+        </div>
+        <div className="rangeTime__btn">
+          <button className="rangeTime__btn__cancel" onClick={handleClearSearch}>Clear search</button>
+          <button className="rangeTime__btn__confirm" onClick={getHistoricalPriceByDate}>Search by date</button>
         </div>
       </div>
     </div>
