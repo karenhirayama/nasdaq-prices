936c6f67875ce9273794551d250e56848793b274
karenhirayama <karenm.hirayama@gmail.com>
2022-06-29 14:00:43 -0300

    Create end-point

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,3 +1,5 @@
 export const getListPerPage = (page: number) => `https://api.stockdata.org/v1/entity/search?exchanges=nasdaq&page=${page}&api_token=${process.env.REACT_APP_API_KEY}`
 
 export const getStockHistoricalData = (symbol: string) => `https://api.stockdata.org/v1/data/eod?sort=asc&symbols=${symbol}&api_token=${process.env.REACT_APP_API_KEY}`
+
+export const getStockHistoricalByDate = (symbol: string, initialDate: string, finalDate: string) =>  `https://api.stockdata.org/v1/data/eod?sort=asc&symbols=${symbol}&date_from=${initialDate}&date_to=${finalDate}&api_token=${process.env.REACT_APP_API_KEY}`
